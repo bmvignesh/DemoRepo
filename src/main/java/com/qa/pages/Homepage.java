00bb3124c79b5fb96598edbf2a0595d6ef95e3fd
bmvignesh <bmvignesh@gmail.com>
2020-07-28 23:00:23 +0530

    first git commit for my selenium code

---
+++ src/main/java/com/qa/pages/Homepage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class Homepage {
+	
+	public void testutil()
+	{
+		System.out.println("test util method");
+	}
+
+}
---
+++ src/test/java/com/qa/test/Loginpage.java
@@ -0,0 +1,11 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class Loginpage {
+	@Test
+	public void logintest() {
+		System.out.println("login page test");
+	}
+
+}
